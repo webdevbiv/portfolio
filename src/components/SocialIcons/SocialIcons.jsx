8a7e73a3ad6421b24a2fa7bc9d24bdd635dc770a
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-27 11:56:48 +0300

    fix-socials

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -4,29 +4,22 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 
 function SocialIcons() {
+  const icons = [
+    { name: "LikedIn", svg: FaLinkedin, color: "bg-[#0077B5]" },
+    { name: "Github", svg: FaGithub, color: "bg-[#211F1F]" },
+    { name: "Email", svg: HiOutlineMail, color: "bg-[#6fc2b0]" },
+    { name: "Contact", svg: BsFillPersonLinesFill, color: "bg-[#565f69]" },
+  ];
   return (
     <div className="fixed left-0 top-[35%] hidden flex-col lg:flex">
       <ul>
-        <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between bg-[#0077B5] duration-200 hover:ml-[-10px]">
-          <a className="flex w-full items-center justify-between text-gray-300" href="/">
-            LikedIn <FaLinkedin size={30} />
-          </a>
-        </li>
-        <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between bg-[#211F1F] duration-200 hover:ml-[-10px]">
-          <a className="flex w-full items-center justify-between text-gray-300" href="/">
-            Github <FaGithub size={30} />
-          </a>
-        </li>
-        <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between bg-[#6fc2b0] duration-200 hover:ml-[-10px]">
-          <a className="flex w-full items-center justify-between text-gray-300" href="/">
-            Email <HiOutlineMail size={30} />
-          </a>
-        </li>
-        <li className="ml-[-100px] flex h-[60px] w-[160px] items-center justify-between bg-[#565f69] duration-200 hover:ml-[-10px]">
-          <a className="flex w-full items-center justify-between text-gray-300" href="/">
-            Email <BsFillPersonLinesFill size={30} />
-          </a>
-        </li>
+        {icons.map((icon, index) => (
+          <li key={index} className={`${icon.color} ml-[-100px] flex h-[60px] w-[160px] px-4 items-center justify-between  duration-200 hover:ml-[-10px]`}>
+            <a className="flex w-full items-center justify-between text-gray-300" href="/">
+              {icon.name} <icon.svg size={30} />
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
