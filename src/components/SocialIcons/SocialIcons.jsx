dbcceefc85317a7d5f728b7633f5d749837f5615
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-28 18:45:29 +0300

    fix-social-icons

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -7,15 +7,19 @@ function SocialIcons() {
   const icons = [
     { name: "LikedIn", svg: FaLinkedin, color: "bg-[#0077B5]" },
     { name: "Github", svg: FaGithub, color: "bg-[#211F1F]" },
-    { name: "Email", svg: HiOutlineMail, color: "bg-[#6fc2b0]" },
+    { name: "Email", svg: HiOutlineMail, color: "bg-[#16a34a]" },
     { name: "Contact", svg: BsFillPersonLinesFill, color: "bg-[#565f69]" },
   ];
   return (
-    <div className="fixed left-0 top-[35%] hidden flex-col lg:flex">
+    <div
+      className="fixed left-0 top-[35%] 
+    hidden
+    flex-col lg:flex"
+    >
       <ul>
         {icons.map((icon, index) => (
-          <li key={index} className={`${icon.color} ml-[-100px] flex h-[60px] w-[160px] px-4 items-center justify-between  duration-200 hover:ml-[-10px]`}>
-            <a className="flex w-full items-center justify-between text-gray-300" href="/">
+          <li key={index} className={`${icon.color}  ml-[-80px] flex h-[60px] w-[140px] mx-0 items-center justify-between  duration-200 hover:ml-[-8px]`}>
+            <a className="flex px-4 w-full h-full items-center justify-between font-bold text-gray-300" href="/">
               {icon.name} <icon.svg size={30} />
             </a>
           </li>
